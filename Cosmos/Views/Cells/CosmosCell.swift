db5c5caff8dd9e71f4d481567ea51ba005244df6
Samuel Yanez <samuelyanez94@gmail.com>
2019-09-01 17:33:46 -0700

    Delete base cell

--- Cosmos/Views/Cells/CosmosCell.swift
+++ Cosmos/Views/Cells/CosmosCell.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 
-// TODO: Combine Cosmos cell and base cell into one
-
-class CosmosCell: BaseCell {
+class CosmosCell: UICollectionViewCell {
     
     static let identifier = "com.samuelyanez.CosmosCell"
     
+    static let height: CGFloat = 450
+    
     static let cornerRadius: CGFloat = 20
+    
+    var feedbackGenerator: UISelectionFeedbackGenerator?
     
     /// Image View
     @IBOutlet var imageView: UIImageView!
@@ -30,6 +32,11 @@ class CosmosCell: BaseCell {
     
     /// Date Label
     @IBOutlet var dateLabel: UILabel!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        configureGestureRecognizer()
+    }
     
     override func draw(_ rect: CGRect) {
         setupShadow()
@@ -53,5 +60,41 @@ class CosmosCell: BaseCell {
     
     private func setupBorderRadius() {
         containerView.layer.cornerRadius = CosmosCell.cornerRadius
+    }
+}
+
+// MARK: - Gesture Recognizer
+
+extension CosmosCell {
+        
+    private func configureGestureRecognizer() {
+        let longPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPressGesture(gestureRecognizer:)))
+        longPressGestureRecognizer.minimumPressDuration = 0.25
+        addGestureRecognizer(longPressGestureRecognizer)
+    }
+    
+    @objc internal func handleLongPressGesture(gestureRecognizer: UILongPressGestureRecognizer) {
+        if gestureRecognizer.state == .began {
+            handleLongPressBegan()
+        } else if gestureRecognizer.state == .ended || gestureRecognizer.state == .cancelled {
+            handleLongPressEnded()
+        }
+    }
+    
+    private func handleLongPressBegan() {
+        feedbackGenerator = UISelectionFeedbackGenerator()
+        feedbackGenerator?.prepare()
+        UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.2, options: .beginFromCurrentState, animations: {
+            self.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+            self.feedbackGenerator?.selectionChanged()
+        }, completion: { _ in
+            self.feedbackGenerator = nil
+        })
+    }
+    
+    private func handleLongPressEnded() {
+        UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.2, options: .beginFromCurrentState, animations: {
+            self.transform = CGAffineTransform.identity
+        }, completion: nil)
     }
 }
