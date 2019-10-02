0c98bbd6a0403b11ae110aed7769f32a8c114147
Samuel Yanez <samuelyanez94@gmail.com>
2019-10-01 23:53:21 -0700

    Add the first unit test! 🚀

---
+++ CosmosTests/Networking/CosmosClientTest.swift
@@ -0,0 +1,40 @@
+//
+//  CosmosClientTest.swift
+//  CosmosTests
+//
+//  Created by Samuel Yanez on 10/1/19.
+//  Copyright © 2019 Samuel Yanez. All rights reserved.
+//
+
+import XCTest
+
+class CosmosClientTest: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testSuccessfulResponse() {
+        
+        let client = MockClient()
+        var apods: [APOD]?
+        let promise = expectation(description: "Successful response.")
+        
+        client.fetch(count: 10) { result in
+            switch result {
+            case .failure:
+                XCTFail("Error found.")
+            case .success(let apodsFromResponse):
+                apods = apodsFromResponse
+            }
+            promise.fulfill()
+        }
+        wait(for: [promise], timeout: 5)
+        
+        XCTAssert(apods != nil)
+    }
+}
